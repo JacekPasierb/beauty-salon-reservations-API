3b57687aaa8563650fc1842197ce02792b41bcb4
Jacek Pasierb <jack_byk@o2.pl>
2023-08-07 14:12:03 +0200

    Configure route/api router.get for 'by-date', 'by-client' , 'by-cosmetician'

--- models/appointments.js
+++ models/appointments.js
@@ -12,12 +12,74 @@ const listAppointments = async () => {
   }
 };
 
+const getAppointmentsByDate = async (date) => {
+  try {
+    const appointmentsData = await listAppointments();
+    const filteredAppointments = appointmentsData.filter((appointment) => {
+      return appointment.appointmentDateTime.startsWith(date);
+    });
+    return filteredAppointments;
+  } catch (error) {
+    console.error("Błąd podczas pobierania rezerwacji:", error);
+  }
+};
+const getAppointmentsByDateTime = async (dateTime) => {
+  try {
+    const appointmentsData = await listAppointments();
+    const filteredAppointments = appointmentsData.filter((appointment) => {
+      return appointment.appointmentDateTime.startsWith(dateTime);
+    });
+    return filteredAppointments;
+  } catch (error) {
+    console.error("Błąd podczas pobierania rezerwacji:", error);
+  }
+};
+
+const getAppointmentsByClientName = async (firstName, lastName) => {
+  try {
+    const appointmentsData = await listAppointments();
+    const filteredAppointments = appointmentsData.filter((appointment) => {
+      return (
+        appointment.client.firstName.toLowerCase() ===
+          firstName.toLowerCase() &&
+        appointment.client.lastName.toLowerCase() === lastName.toLowerCase()
+      );
+    });
+
+    return filteredAppointments;
+  } catch (error) {
+    throw new Error("Błąd podczas pobierania rezerwacji.");
+  }
+};
+
+const getAppointmentsByCosmeticianName = async (firstName, lastName) => {
+  try {
+    const appointmentsData = await listAppointments();
+    const filteredAppointments = appointmentsData.filter((appointment) => {
+      return (
+        appointment.cosmetician.firstName.toLowerCase() ===
+          firstName.toLowerCase() &&
+        appointment.cosmetician.lastName.toLowerCase() ===
+          lastName.toLowerCase()
+      );
+    });
+
+    return filteredAppointments;
+  } catch (error) {
+    throw new Error("Błąd podczas pobierania rezerwacji.");
+  }
+};
+
 const deleteAppointment = async (appointmentsId) => {};
 
 const addAppointment = async (newAppointment) => {};
 
 module.exports = {
   listAppointments,
+  getAppointmentsByDate,
+  getAppointmentsByDateTime,
+  getAppointmentsByClientName,
+  getAppointmentsByCosmeticianName,
   deleteAppointment,
   addAppointment,
 };
--- routes/api/appointments.js
+++ routes/api/appointments.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { listAppointments } = require("../../models/appointments");
+const { listAppointments, getAppointmentsByDate, getAppointmentsByDateTime, getAppointmentsByClientName, getAppointmentsByCosmeticianName } = require("../../models/appointments");
 
 const router = express.Router();
 
@@ -9,6 +9,64 @@ router.get("/", async (req, res) => {
     res.status(200).json(appointments);
   } catch (error) {
     console.error("Error handling GET /api/appointments:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/by-date", async (req, res, next) => {
+  try {
+    const time = req.query.time;
+    if (time) {
+      return next();
+    }
+    const date = req.query.date;
+    const appointmentsDate = await getAppointmentsByDate(date);
+    res.status(200).json(appointmentsDate);
+  } catch (error) {
+    console.error("Error handling GET /api/appointments/by-date", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+router.get("/by-date", async (req, res) => {
+  try {
+    const date = req.query.date;
+    const time = req.query.time;
+    const dateTime = `${date}T${time}`;
+    console.log(dateTime);
+
+    const appointmentsDateTime = await getAppointmentsByDateTime(dateTime);
+    res.status(200).json(appointmentsDateTime);
+  } catch (error) {
+    console.error("Error handling GET /api/appointments/by-date", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/by-client", async (req, res) => {
+  try {
+    const firstName = req.query.firstName;
+    const lastName = req.query.lastName;
+
+    const appointments = await getAppointmentsByClientName(firstName, lastName);
+    res.status(200).json(appointments);
+  } catch (error) {
+    console.error("Error handling GET /api/appointments/by-client", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/by-cosmetician", async (req, res) => {
+  try {
+    const firstName = req.query.firstName;
+    const lastName = req.query.lastName;
+
+    const appointmentsByCosmetician = await getAppointmentsByCosmeticianName(
+      firstName,
+      lastName
+    );
+    res.status(200).json(appointmentsByCosmetician);
+  } catch (error) {
+    console.error("Error handling GET /api/appointments/by-cosmetician", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
