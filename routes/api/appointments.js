a071625c092e11a5f850d74addff997204ac12bd
Jacek Pasierb <jack_byk@o2.pl>
2023-08-07 14:43:10 +0200

    Install lib Joi and create validation for route.post

--- routes/api/appointments.js
+++ routes/api/appointments.js
@@ -1,5 +1,40 @@
 const express = require("express");
-const { listAppointments, getAppointmentsByDate, getAppointmentsByDateTime, getAppointmentsByClientName, getAppointmentsByCosmeticianName, addAppointment, deleteAppointment } = require("../../models/appointments");
+const Joi = require("joi");
+
+const appointmentSchema = Joi.object({
+  client: Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string()
+      .pattern(
+        /^(\+\d{2,3}\s?)?(\d{3}[\s-]?\d{3}[\s-]?\d{3}|\d{2}[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2})$/
+      )
+      .required(),
+  }).required(),
+
+  appointmentDateTime: Joi.string().isoDate().required(),
+
+  service: Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required(),
+
+  cosmetician: Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+  }).required(),
+});
+
+const {
+  listAppointments,
+  getAppointmentsByDate,
+  getAppointmentsByDateTime,
+  getAppointmentsByClientName,
+  getAppointmentsByCosmeticianName,
+  addAppointment,
+  deleteAppointment,
+} = require("../../models/appointments");
 
 const router = express.Router();
 
@@ -72,30 +107,36 @@ router.get("/by-cosmetician", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    try {
-      const newAppointment = req.body;
-      const addedAppointment = await addAppointment(newAppointment);
-      res.status(201).json(addedAppointment);
-    } catch (error) {
-      console.error("Error handling POST /api/appointments", error);
-      res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const newAppointment = req.body;
+    const { error } = appointmentSchema.validate(newAppointment);
+    if (error) {
+      return res
+        .status(400)
+        .json({ message: "Validation error", details: error.details });
     }
+    const addedAppointment = await addAppointment(newAppointment);
+    res.status(201).json(addedAppointment);
+  } catch (error) {
+    console.error("Error handling POST /api/appointments", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-    try {
-      const appointmentId = req.params.id;
+  try {
+    const appointmentId = req.params.id;
 
-      const isDeleted = await deleteAppointment(appointmentId);
-      if (isDeleted) {
-        return res.status(200).json({ message: "contact deleted" });
-      } else {
-        return res.status(404).json({ message: "Not found" });
-      }
-    } catch (error) {
-      console.error("Error handling DELETE /api/appointments/:id", error);
-      res.status(500).json({ error: "Internal Server Error" });
+    const isDeleted = await deleteAppointment(appointmentId);
+    if (isDeleted) {
+      return res.status(200).json({ message: "contact deleted" });
+    } else {
+      return res.status(404).json({ message: "Not found" });
     }
+  } catch (error) {
+    console.error("Error handling DELETE /api/appointments/:id", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
