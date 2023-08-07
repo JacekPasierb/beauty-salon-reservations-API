06537203ec8f239a3442ed05c3ef58847acb8901
Jacek Pasierb <jack_byk@o2.pl>
2023-08-07 13:24:09 +0200

    Create app: install lib express,morgan,cors. Add service for error. Create appointmentsRoute

---
+++ models/appointments.js
@@ -0,0 +1,13 @@
+const listAppointments = async () => { }
+
+const deleteAppointment = async (appointmentsId) => { }
+
+const addAppointment = async (newAppointment) => { }
+
+
+module.exports = {
+  listAppointments,
+  deleteAppointment,
+  addAppointment,
+};
+
