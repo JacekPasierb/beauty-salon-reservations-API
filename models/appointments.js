a194befedcf285c5e03b160ac4ca2961a0836a39
Jacek Pasierb <jack_byk@o2.pl>
2023-08-07 13:49:03 +0200

    Configure route/api router.get '/'

--- models/appointments.js
+++ models/appointments.js
@@ -1,13 +1,23 @@
-const listAppointments = async () => { }
+const path = require("path");
+const fs = require("fs/promises");
 
-const deleteAppointment = async (appointmentsId) => { }
+const listAppointments = async () => {
+  try {
+    const filePath = path.join(__dirname, "appointments.json");
+    const fileContent = await fs.readFile(filePath, { encoding: "utf8" });
+    return JSON.parse(fileContent);
+  } catch (error) {
+    console.error("Error reading appointments file:", error);
+    return [];
+  }
+};
 
-const addAppointment = async (newAppointment) => { }
+const deleteAppointment = async (appointmentsId) => {};
 
+const addAppointment = async (newAppointment) => {};
 
 module.exports = {
   listAppointments,
   deleteAppointment,
   addAppointment,
 };
-
--- routes/api/appointments.js
+++ routes/api/appointments.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
+const { listAppointments } = require("../../models/appointments");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const appointments = await listAppointments();
+    res.status(200).json(appointments);
+  } catch (error) {
+    console.error("Error handling GET /api/appointments:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-router.post("/", async (req, res) => { });
+router.post("/", async (req, res) => {});
 
-router.delete("/:id", async (req, res) => { });
+router.delete("/:id", async (req, res) => {});
 
 module.exports = router;
